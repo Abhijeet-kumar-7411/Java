cab4af8aa46a1978473b976e5f67d9bba153d9e2
Abhijeet Kumar <abhijeet7411@gmail.com>
2025-05-27 21:59:36 +0530

    V1 for odd and even

---
+++ odd_even.java
@@ -0,0 +1,14 @@
+public class odd_even {
+    public static void main(String[] args){
+      int a=3;
+
+      if (a%2==0)
+      System.out.println( a + "Number is even");
+
+      else
+      System.out.println( a + "Number is odd");
+
+
+    }
+    
+}
